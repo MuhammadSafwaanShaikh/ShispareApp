a98fbace8a59623ead7b9182ac95cd3314879815
MuhammadSafwaanShaikh <safwaan.shaikh@shispare.com>
2023-10-03 17:34:17 +0500

    Created : header&sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { LoginComponent } from './login/login.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
